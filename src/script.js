a554663fbf9081f604972e93e52e8162d60bb41b
williampol <103951515+williampol@users.noreply.github.com>
2022-07-19 15:03:42 -0400

    Clock works now
    The animation for the clock is fully running and syncs with the current time. TODO make page consistent with the other pages.

--- src/script.js
+++ src/script.js
@@ -1,1 +1,33 @@
 "use strict";
+
+const secHand = document.getElementById("sec-hand");
+const midHand = document.getElementById("min-hand");
+const hourHand = document.getElementById("hour-hand");
+
+updateSec(secHand);
+updateMin(midHand);
+updateHour(hourHand);
+
+function updateSec(image) {
+    let date = new Date(Date.now());
+    let angle = date.getSeconds() * 6;
+
+    image.setAttribute("style", "transform: rotate(" + angle + "deg)");
+    setTimeout(() => updateSec(image), 1000);
+}
+
+function updateMin(image) {
+    let date = new Date(Date.now());
+    let angle = date.getMinutes() * 6 + date.getSeconds() / 60;
+   
+    image.setAttribute("style", "transform: rotate(" + angle + "deg)");
+    setTimeout(() => updateMin(image), 1000);
+}
+
+function updateHour(image) {
+    let date = new Date(Date.now());
+    let angle = date.getHours() * 6 + date.getMinutes() / 60 + date.getSeconds() / 3600;
+   
+    image.setAttribute("style", "transform: rotate(" + angle + "deg)");
+    setTimeout(() => updateHour(image), 1000);
+}
